270a2958379cdcd1ba55068cec423ff3067b87f0
Bruno Hass <bruno_hass@live.com>
2019-10-03 18:32:27 -0300

    Adds primitive operations to AdjacencyMap

--- src/main/scala/alga/AdjacencyMap.scala
+++ src/main/scala/alga/AdjacencyMap.scala
@@ -1,17 +1,14 @@
 package alga
+import scalaz._, Scalaz._
 
-sealed trait AdjacencyMap[+A] {
-
-}
-
-final case class AdjacencyMap[A](adjacencyMap: Map[A, Set[A]]) extends AdjacencyMap[A]
+final case class AdjacencyMap[A](adjacencyMap: Map[A, Set[A]])
 
 object AdjacencyMap {
   /** Construct the ''empty'' adjacency map.
    *   Complexity: '''O(1)''' time, memory and size.
    *   @return the empty adjacency map.
    */
-  def empty: AdjacencyMap[Nothing] = AdjacencyMap(Map.empty)
+  def empty[A]: AdjacencyMap[A] = AdjacencyMap(Map())
 
   /** Construct the adjacency map comprising a ''single isolated vertex''. An alias
    *   for the constructor [[Vertex]]. Complexity: '''O(1)''' time, memory and
@@ -19,20 +16,19 @@ object AdjacencyMap {
    *   @param v the vertex of type `A`.
    *   @return  the adjacency map comprising a single vertex '''v'''.
    */
-  def vertex[A](a: A) =  AdjacencyMap(Map(a -> Set.empty))
+  def vertex[A](a: A): AdjacencyMap[A] =  AdjacencyMap(Map(a -> Set.empty))
 
-  /** Construct the graph comprising a ''single isolated vertex''. An alias
-   *   for the constructor [[Vertex]]. Complexity: '''O(1)''' time, memory and
-   *   size.
-   *   @param v the vertex of type `A`.
-   *   @return  the graph comprising a single vertex '''v'''.
-   */
   /**
    * Construct the adjacency map comprising of a ''single edge''
    * Complexity: '''O(1)''' time, memory and
    * *   size.
    */
-  def edge[A](x: A, y: A) =
+  def edge[A](x: A, y: A): AdjacencyMap[A] =
     if (x == y) AdjacencyMap(Map(x -> Set(y)))
-    else AdjacencyMap(Map(x -> Set(y), y -> Set.empty))
+    else AdjacencyMap(Map(x -> Set(y), y -> Set[A]()))
+
+  def overlay[A](x: AdjacencyMap[A], y: AdjacencyMap[A]): AdjacencyMap[A] =
+    (x, y) match {
+      case (AdjacencyMap(x), AdjacencyMap(y)) => AdjacencyMap[A](x |+| y)
+    }
 }
